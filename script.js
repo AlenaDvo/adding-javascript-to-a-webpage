1834de744e061eea801052f40104e6d05d5d7c7d
AlenaDvo <131749753+AlenaDvo@users.noreply.github.com>
2023-05-12 13:01:54 +0200

    intro to jquery - exercise 2

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 console.log("Hello Alena");
+
+let color = "purple";
+let number = 10;
+let word = "cool";
+
+if (color === "purple") {
+    $(".first").css("background-color", color);
+};
+
+if (number > 100) {
+    $(".second").text("whoa, that's a big number");
+} else {
+    $(".second").text("just a regular number, please");
+};
+
+if (word === "cool") {
+    $(".third").text("Power of DOM");
+} else {
+    $(".fourth").text("Power of DOM");
+};
